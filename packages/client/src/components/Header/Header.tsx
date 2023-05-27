92fa2928682fbdeff8da131054a0eb8255b26934
PB-Liu <mindware2000@gmail.com>
2023-05-27 13:17:59 -0400

    update header

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import logoUrl from '../../assets/bc-logo2.png'
 
 const Header = () => {
   return (
-    <header className="flex justify-center flex-wrap flex-col phone:flex-row phone:justify-between">
+    <header className="flex justify-center flex-wrap flex-col phone:flex-row phone:justify-between maxW">
       <div className = "flex justify-center items-center p-2">
           <img src={logoUrl} className="h-8 phone:h-10"></img>
           <span className="font-semibold font-mono text-gray-600 phone:text-2xl tracking-tight pl-2">Byte-Chef</span>
