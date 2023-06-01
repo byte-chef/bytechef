a1f07cff09753f718b4f3fa91d5420c5b3bed03d
Martin Carmona <martin.e.carmona@gmail.com>
2023-05-31 18:02:36 -0700

    add error handling to save recipe method

--- packages/server/src/controllers/recipeController.ts
+++ packages/server/src/controllers/recipeController.ts
@@ -5,16 +5,40 @@ const saveRecipe = async (req: Request, res: Response, next: NextFunction) => {
   const { generatedRecipe } = res.locals;
   const { user } = req;
 
-  const savedRecipe = RecipeModel.build({
-    ...generatedRecipe,
-    userId: user.id,
-  });
+  if (!generatedRecipe) {
+    return next({
+      log: `Error retrieving generated recipe: ${generatedRecipe}`,
+      message: 'Server Error',
+      status: 500,
+    });
+  }
 
-  savedRecipe.save();
+  if (!user) {
+    return next({
+      log: `Error retrieving user: ${user}`,
+      message: 'Server Error',
+      status: 500,
+    });
+  }
 
-  res.locals.savedRecipe = savedRecipe;
+  try {
+    const savedRecipe = RecipeModel.build({
+      ...generatedRecipe,
+      userId: user.id,
+    });
 
-  return next();
+    savedRecipe.save();
+
+    res.locals.savedRecipe = savedRecipe;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: `Error saving recipe to db: ${error}`,
+      message: 'Server Error',
+      status: 500,
+    });
+  }
 };
 
 export default {
